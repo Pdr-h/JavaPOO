3321f41c7980fe6981cf02f97ec6a67fbbc248ba
Pdr-h <77487395+Pdr-h@users.noreply.github.com>
2024-04-25 18:00:13 -0300

    Update Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -12,6 +12,7 @@ public class Empresa {
 
 public static void main (String[]args){
 Fornecedor fornecedor = new Fornecedor();
+Cliente cliente = new Cliente();
 
 String nomeEmpresa = JOptionPane.showInputDialog("Digite o nome da Empresa!");
 fornecedor.ProdutoFornecido(nomeEmpresa);
@@ -20,7 +21,7 @@ Object[] opcoes = {"CLIENTE", "FUNCIONÁRIO"};
 int opcaoSlct = JOptionPane.showOptionDialog(null, "Selecione uma Opção", "OPÇÕES", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]); 
 
 if(opcaoSlct == 0){
-    JOptionPane.showMessageDialog(null, "OI ");
+    cliente.cadastroCliente();
 }
 }
 
